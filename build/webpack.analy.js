53920bad05c12feee4fcb0806d41ed1f2a30a1b6
whatliu1 <41365701+whatliu1@users.noreply.github.com>
2023-06-27 14:05:15 +0800

    1 binaries uploaded

--- build/webpack.analy.js
+++ build/webpack.analy.js
@@ -3,15 +3,18 @@ const SpeedMeasurePlugin = require('speed-measure-webpack-plugin'); // 引入web
 const smp = new SpeedMeasurePlugin(); // 实例化分析插件
 const { merge } = require('webpack-merge'); // 引入合并webpack配置方法
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // 引入分析打包结果插件
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-console.log(prodConfig);
+// // 使用smp.wrap方法,把生产环境配置传进去,由于后面可能会加分析配置,所以先留出合并空位
+// module.exports = smp.wrap(
+//   merge(prodConfig, {
+//     plugins: [
+//       new BundleAnalyzerPlugin(), // 配置分析打包结果插件
+//     ],
+//   })
+// );
 
-// 使用smp.wrap方法,把生产环境配置传进去,由于后面可能会加分析配置,所以先留出合并空位
-module.exports = smp.wrap(
-  merge(prodConfig, {
-    plugins: [
-      new BundleAnalyzerPlugin(), // 配置分析打包结果插件
-    ],
-  })
-);
+module.exports = merge(prodConfig, {
+  plugins: [
+    new BundleAnalyzerPlugin(), // 配置分析打包结果插件
+  ],
+});
